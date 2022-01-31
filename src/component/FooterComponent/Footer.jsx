c3e41cb4e971066308cdb4c75a5b810437cadda7
EdenGenetTasama <edentasama19@gmail.com>
2022-01-31 15:40:22 +0200

    add grid templet

--- src/component/FooterComponent/Footer.jsx
+++ src/component/FooterComponent/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './footer.css';
 
 const Footer = () => {
   return (
--- src/component/NavbarComponent/Navbar.jsx
+++ src/component/NavbarComponent/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import './navbar.css';
+
 
 const Navbar = () => {
   return (
